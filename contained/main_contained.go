e02a112186f3eb870d3b1e78ced1d49e4d26319e
Nada <nada.de@gmail.com>
2025-04-20 20:58:37 +0200

    add contained and uncontained versions + Vagrantfile

---
+++ contained/main_contained.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"strconv"
+	"syscall"
+)
+
+func main() {
+	switch os.Args[1] {
+	case "run":
+		host()
+	case "container":
+		container()
+	default:
+		panic("nope")
+	}
+}
+
+func host() {
+	fmt.Printf("Running %v\n", os.Args[2:])
+
+	cmd := exec.Command("/proc/self/exe", append([]string{"container"}, os.Args[2:]...)...)
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	cmd.SysProcAttr = &syscall.SysProcAttr{
+		Cloneflags:  syscall.CLONE_NEWUTS | syscall.CLONE_NEWPID | syscall.CLONE_NEWNS | syscall.CLONE_NEWUSER,
+		Credential:  &syscall.Credential{Uid: 0, Gid: 0},
+		UidMappings: []syscall.SysProcIDMap{{ContainerID: 0, HostID: 1000, Size: 1}},
+		GidMappings: []syscall.SysProcIDMap{{ContainerID: 0, HostID: 1000, Size: 1}},
+	}
+
+	//create the new cgroup
+	cg_path := "/sys/fs/cgroup/"
+	my_cg_path := filepath.Join(cg_path, "container_gophercamp")
+	must(os.MkdirAll(my_cg_path, 0755))
+
+	//limit max amount of processes to 20
+	must(os.WriteFile(filepath.Join(my_cg_path, "pids.max"), []byte("20"), 0700))
+
+	//add this process to the new cgroup
+	must(os.WriteFile(filepath.Join(my_cg_path, "cgroup.procs"), []byte(strconv.Itoa(os.Getpid())), 0700))
+
+	must(cmd.Run())
+}
+
+func container() {
+	fmt.Printf("Running %v in a container\n", os.Args[2:])
+
+	cmd := exec.Command(os.Args[2], os.Args[3:]...)
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	//set hostname within the container
+	must(syscall.Sethostname([]byte("container")))
+
+	//change root to the new file system (downloaded and untared in the vagrant provisioning shell)
+	must(syscall.Chroot("/home/vagrant/ubuntufs"))
+
+	//after chroot, we need to explicitly change directory to /
+	must(syscall.Chdir("/"))
+
+	//mount the procfs, otherwise /proc is empty
+	must(syscall.Mount("proc", "proc", "proc", 0, ""))
+	must(cmd.Run())
+
+	must(syscall.Unmount("proc", 0))
+}
+
+func must(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
